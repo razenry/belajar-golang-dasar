81e9c32f45b6fcea696fd838278c970b69cfdae6
Razenry <razenry.code@gmail.com>
2025-04-08 00:29:28 -0400

    Update variable.go

--- variable.go
+++ variable.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	
+
 	var name string = "Kafkariela Prima Rasendriya"
 	var age int = 20
 	var isMarried bool = false
@@ -13,25 +13,28 @@ func main() {
 	fmt.Println("Umur : ", age)
 	fmt.Println("Jenis Kelamin : ", gender)
 	fmt.Println("Menikah : ", isMarriedCheck(isMarried))
-	
+
 	name = "Eko Ari Iswanto"
 	age = 38
 	isMarried = true
-	
+
 	fmt.Println("Nama : ", name)
 	fmt.Println("Umur : ", age)
 	fmt.Println("Jenis Kelamin : ", gender)
 	fmt.Println("Menikah : ", isMarriedCheck(isMarried))
-	
+
 	name = "Ela Asmani"
 	age = 48
 	gender = "Perempuan"
 	isMarried = true
-	
+
 	fmt.Println("Nama : ", name)
 	fmt.Println("Umur : ", age)
 	fmt.Println("Jenis Kelamin : ", gender)
 	fmt.Println("Menikah : ", isMarriedCheck(isMarried))
+
+	name = "Razenry Aretha Silverine"
+	fmt.Println(sayHello(name, "Siang"))	
 }
 
 func isMarriedCheck(isMarried bool) string {
@@ -39,4 +42,10 @@ func isMarriedCheck(isMarried bool) string {
 		return "Ya"
 	}
 	return "Tidak"
+}
+
+func sayHello(name string, time string) string {
+	greating := "Selamat " + time	
+
+	return "Hello " + name + " " + greating
 }
